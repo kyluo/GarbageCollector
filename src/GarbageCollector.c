600f5a34b7b6b1a4d0410496bb7779caf0321002
Ruipeng Han <ruipenghan@Ruipengs-MacBook-Pro.local>
2021-02-21 00:07:01 -0600

    Add linked list structure.

--- src/GarbageCollector.c
+++ src/GarbageCollector.c
@@ -1,6 +1,36 @@
 #include "GarbageCollector.h"
+#include <stdio.h>
+#include <string.h>
 
-int main () {
-    puts("Hello World!");
-    return 0;
+/* Initialization of global variables */
+metadata* head = 0;
+size_t total_memory_used = 0;
+size_t total_memory_freed = 0;
+
+void add_to_metadata_list(metadata* new_metadata, size_t request_size) {
+    new_metadata->size = request_size;
+    // total_memory_requested += request_size;
+    /* If head is null, just assign it to the new meta data*/
+    if (head == NULL) {
+        head = new_metadata;
+        head->next = NULL;
+        return;
+    }
+
+    /* If not, then use a temp to make new meta data the new head and link the old head*/
+    metadata* temp = head;
+    head = new_metadata;
+    new_metadata->next = temp;
+}
+
+void *mini_malloc(size_t request_size) {
+    return NULL;
+}
+
+void *mini_calloc(size_t num_elements, size_t element_size) {
+    return NULL;
+}
+                  
+void *mini_realloc(void *ptr, size_t request_size) {
+    return NULL;
 }
--- src/GarbageCollector.h
+++ src/GarbageCollector.h
@@ -6,5 +6,23 @@
 #include <stdlib.h>
 #include <stdio.h>
 typedef struct {
+    // Number of bytes this piece of memory holds (excluding the metadata size)
+    size_t size;
+
+    // Pointer to the next instance of meta_data in the list
+    struct _meta_data *next;
+} metadata;
+
+typedef struct {
     size_t size; 
 } GarbageCollector;
+
+extern metadata* head;
+extern size_t total_memory_used;
+extern size_t total_memory_freed;
+
+
+void add_to_metadata_list(metadata* new_metadata, size_t request_size);
+void *mini_malloc(size_t request_size);
+void *mini_calloc(size_t num_elements, size_t element_size);
+void *mini_realloc(void *ptr, size_t request_size);
